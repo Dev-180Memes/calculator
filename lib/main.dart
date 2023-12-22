8621d8cd7d478e9657827ce2a0b0eb90e8264c6b
Adeoluwa Agbakosi <adeoluwaagbakosi@gmail.com>
2023-12-22 20:49:38 +0100

    Completed calculator app

---
+++ lib/main.dart
@@ -0,0 +1,269 @@
+import "package:flutter/material.dart";
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Calculator(),
+    );
+  }
+}
+
+class Calculator extends StatefulWidget {
+  const Calculator({super.key});
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+
+  Widget calcbutton( String btntxt, Color btncolor, Color txtcolor) {
+    return ElevatedButton(
+      onPressed: () {
+        calculation(btntxt);
+      },
+      style: ButtonStyle(
+        shape: MaterialStateProperty.all(const CircleBorder()),
+        backgroundColor: MaterialStateProperty.all(btncolor),
+        padding: MaterialStateProperty.all(const EdgeInsets.all(20)),
+        // Add textStyle if you need to directly apply text style to the button's text
+        textStyle: MaterialStateProperty.all(
+          TextStyle(
+            fontSize: 35,
+            color: txtcolor, // Ensure the text color works with the ElevatedButton's foreground color
+          ),
+        ),
+      ),
+      child: Text(
+        btntxt,
+        style: TextStyle(
+          fontSize: 35,
+          color: txtcolor,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(title: const Text("Calculator"), backgroundColor: Colors.black),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 5),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      text,
+                      textAlign: TextAlign.left,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 100,
+                      ),
+                    ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // Here buttons functions will be called
+                calcbutton('AC', Colors.grey, Colors.black),
+                calcbutton('+/-', Colors.grey, Colors.black),
+                calcbutton('%', Colors.grey, Colors.black),
+                calcbutton('/', Colors.amber[700]!, Colors.white),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // Here buttons functions will be called
+                calcbutton('7', Colors.grey[850]!, Colors.white),
+                calcbutton('8', Colors.grey[850]!, Colors.white),
+                calcbutton('9', Colors.grey[850]!, Colors.white),
+                calcbutton('x', Colors.amber[700]!, Colors.white),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // Here buttons functions will be called
+                calcbutton('4', Colors.grey[850]!, Colors.white),
+                calcbutton('5', Colors.grey[850]!, Colors.white),
+                calcbutton('6', Colors.grey[850]!, Colors.white),
+                calcbutton('-', Colors.amber[700]!, Colors.white),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // Here buttons functions will be called
+                calcbutton('1', Colors.grey[850]!, Colors.white),
+                calcbutton('2', Colors.grey[850]!, Colors.white),
+                calcbutton('3', Colors.grey[850]!, Colors.white),
+                calcbutton('+', Colors.amber[700]!, Colors.white),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  style: ButtonStyle(
+                    padding: MaterialStateProperty.all(const EdgeInsets.fromLTRB(34, 20, 128, 20)),
+                    shape: MaterialStateProperty.all(const StadiumBorder()),
+                    backgroundColor: MaterialStateProperty.all(Colors.grey[850]),
+                  ),
+                  onPressed: () {
+                    // button function
+                  },
+                  child: const Text(
+                    '0',
+                    style: TextStyle(
+                      fontSize: 35,
+                      color: Colors.white,
+                    ),
+                  ),
+                  ),
+                calcbutton('.', Colors.grey[850]!, Colors.white),
+                calcbutton('=', Colors.amber[700]!, Colors.white),
+              ]
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  dynamic text ='0';
+  double numOne = 0;
+  double numTwo = 0;
+
+  dynamic result = '';
+  dynamic finalResult = '';
+  dynamic opr = '';
+  dynamic preOpr = '';
+  void calculation(btnText) {
+
+
+    if(btnText  == 'AC') {
+      text ='0';
+      numOne = 0;
+      numTwo = 0;
+      result = '';
+      finalResult = '0';
+      opr = '';
+      preOpr = '';
+
+    } else if( opr == '=' && btnText == '=') {
+
+      if(preOpr == '+') {
+        finalResult = add();
+      } else if( preOpr == '-') {
+        finalResult = sub();
+      } else if( preOpr == 'x') {
+        finalResult = mul();
+      } else if( preOpr == '/') {
+        finalResult = div();
+      }
+
+    } else if(btnText == '+' || btnText == '-' || btnText == 'x' || btnText == '/' || btnText == '=') {
+
+      if(numOne == 0) {
+        numOne = double.parse(result);
+      } else {
+        numTwo = double.parse(result);
+      }
+
+      if(opr == '+') {
+        finalResult = add();
+      } else if( opr == '-') {
+        finalResult = sub();
+      } else if( opr == 'x') {
+        finalResult = mul();
+      } else if( opr == '/') {
+        finalResult = div();
+      }
+      preOpr = opr;
+      opr = btnText;
+      result = '';
+    }
+    else if(btnText == '%') {
+      result = numOne / 100;
+      finalResult = doesContainDecimal(result);
+    } else if(btnText == '.') {
+      if(!result.toString().contains('.')) {
+        result = '$result.';
+      }
+      finalResult = result;
+    }
+
+    else if(btnText == '+/-') {
+      result.toString().startsWith('-') ? result = result.toString().substring(1): result = '-$result';
+      finalResult = result;
+
+    }
+
+    else {
+      result = result + btnText;
+      finalResult = result;
+    }
+
+
+    setState(() {
+      text = finalResult;
+    });
+
+  }
+
+
+  String add() {
+    result = (numOne + numTwo).toString();
+    numOne = double.parse(result);
+    return doesContainDecimal(result);
+  }
+
+  String sub() {
+    result = (numOne - numTwo).toString();
+    numOne = double.parse(result);
+    return doesContainDecimal(result);
+  }
+  String mul() {
+    result = (numOne * numTwo).toString();
+    numOne = double.parse(result);
+    return doesContainDecimal(result);
+  }
+  String div() {
+    result = (numOne / numTwo).toString();
+    numOne = double.parse(result);
+    return doesContainDecimal(result);
+  }
+
+
+  String doesContainDecimal(dynamic result) {
+
+    if(result.toString().contains('.')) {
+      List<String> splitDecimal = result.toString().split('.');
+      if(!(int.parse(splitDecimal[1]) > 0)) {
+        return result = splitDecimal[0].toString();
+      }
+    }
+    return result;
+  }
+}
+
